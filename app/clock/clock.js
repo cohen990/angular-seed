5cca2c661657f9696308b7295fc6a689d2f73155
cohen990 <dan.cohen@mopowered.co.uk>
2014-12-31 16:46:47 +0000

    time updates every second and displays in 24h closes #3

--- app/clock/clock.js
+++ app/clock/clock.js
@@ -10,11 +10,42 @@ angular.module('myApp.clock', ['ngRoute'])
 }])
 
 .controller('ClockCtrl',  ['$scope', function($scope) {
-	$scope.GetTime = function(){
-	    var today = new Date();
-	    var hours = today.getHours();
-	    var minutes = today.getMinutes();
+}])
 
-		return hours + ":" + minutes;
-	};
-}]);
+.directive('myCurrentTime', function($timeout, dateFilter) {
+	// return the directive link function. (compile function not needed)
+	return function(scope, element, attrs) {
+		var format = 'h:mm:ss',  // date format
+		  timeoutId; // timeoutId, so that we can cancel the time updates
+
+		// used to update the UI
+		function updateTime() {
+		element.text(dateFilter(new Date(), format));
+		}
+
+		// watch the expression, and update the UI on change.
+		scope.$watch(attrs.myCurrentTime, function() {
+		format = 'H:mm:ss';
+		updateTime();
+		});
+
+		// schedule update in one second
+		function updateLater() {
+			// save the timeoutId for canceling
+			timeoutId = $timeout(
+				function() {
+						updateTime();
+						updateLater();
+					},
+				1000);
+		}
+
+		// listen on DOM destroy (removal) event, and cancel the next UI update
+		// to prevent updating time ofter the DOM element was removed.
+		element.bind('$destroy', function() {
+		$timeout.cancel(timeoutId);
+		});
+
+		updateLater(); // kick off the UI update process.
+	}
+});
